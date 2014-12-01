20a744949b090fef205d9d0d01afe49377bf8c5d
Ben Letton <bletton@equalexperts.com>
2014-12-01 17:32:42 +0000

    Complete chapter 7 progress

--- fp/src/main/scala/ben/fp/chapter7/Par.scala
+++ fp/src/main/scala/ben/fp/chapter7/Par.scala
@@ -1,17 +1,118 @@
 package ben.fp.chapter7
 
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
+import scala.language.postfixOps
+
+
 object Par {
 
-  def unit[A](a: A): Par[A] = ???
+  type Par[A] = ExecutorService => Future[A]
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+  type TimedPar[A] = ExecutorService => TimedFuture[A]
 
-  def fork[A](a: => Par[A]): Par[A] = ???
+  type CurrentTime = () => Long
 
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def now[A]: Long = System.currentTimeMillis()
 
-  def run[A](a: Par[A]): A = ???
+  def unit[A](a: A): Par[A] =
+    (es: ExecutorService) => UnitFuture(a)
+
+  def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean =
+    p(e).get == p2(e).get
+
+  def timedUnit[A](a: Future[A]): TimedPar[A] =
+    (es: ExecutorService) => TimedFuture(a)
+
+  def map[A, B](a: Par[A])(f: A => B): Par[B] =
+    (es: ExecutorService) => UnitFuture(f(a(es).get))
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def map2WithTimeout[A, B, C](a: TimedPar[A], b: TimedPar[B], timeout: Long, units: TimeUnit)(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val (elapsed, af) = a(es).getWithTimeout(timeout, units)
+      val timeout1: Long = timeout - elapsed
+      val (e2, bf) = b(es).getWithTimeout(timeout1, units)
+      UnitFuture(f(af, bf))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call = a(es).get
+    })
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    ps.foldRight[Par[List[A]]](unit(List()))((b, a) => map2(b, a)(_ :: _))
+
+  def sequence_1st_attempt[A](ps: List[Par[A]]): Par[List[A]] =
+    (es: ExecutorService) => {
+      UnitFuture(ps.foldRight(List.empty[A])((b, a) => b(es).get +: a))
+    }
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] =
+    sequence(ps.map(asyncF(f)))
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+
+    val toList: A => List[A] = (a: A) => if (f(a)) List(a) else Nil
+
+    map(sequence(as.map(asyncF(toList))))(_.flatten)
+  }
+
+  def lazyUnit[A](a: => A): Par[A] =
+    fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    (a: A) => lazyUnit(f(a))
+
+
+  case class UnitFuture[A](get: A) extends Future[A] {
+
+    def isDone = true
+
+    def get(timeout: Long, units: TimeUnit): A = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  case class TimedFuture[A](a: Future[A]) extends Future[A] {
+
+    private val ref: AtomicReference[Option[A]] = new AtomicReference(None)
+
+    private def value  = ref.get()
+
+    private def getValue: Option[A] = ref.get
+
+    private def setValue(a: A): A = value getOrElse {
+      ref.set(Some(a))
+      value get
+    }
+
+    private def timed(timeout: Long, units: TimeUnit)(future: => A): (Long, A) = {
+      val start = now
+      val v = future
+      (now - start) -> v
+    }
+
+    override def isDone = value.isDefined
+
+    override def get(timeout: Long, unit: TimeUnit): A = setValue(a.get(timeout, unit))
+
+    override def get: A = setValue(a.get(5, TimeUnit.SECONDS))
+
+    def getWithTimeout(timeout: Long, units: TimeUnit): (Long, A) = timed(timeout, units)( get(timeout, units) )
+
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = a.cancel(mayInterruptIfRunning)
+
+    override def isCancelled: Boolean = a.isCancelled
+  }
 }
-
-
-case class Par[A]()
--- fp/src/test/scala/ben/fp/chapter7/Chapter7Spec.scala
+++ fp/src/test/scala/ben/fp/chapter7/Chapter7Spec.scala
@@ -1,22 +1,148 @@
 package ben.fp.chapter7
 
-import org.scalatest.{WordSpec, Matchers}
+import java.util.concurrent._
 
-class Chapter7Spec  extends WordSpec with Matchers {
+import ben.fp.chapter7.Par._
+import org.scalatest.{Matchers, WordSpec}
+
+class Chapter7Spec extends WordSpec with Matchers {
+
+  def es = new ForkJoinPool()
+  def timeout(l:Long) = l
 
 
-  "" should {
+  val parA: Par[Int] = unit(2)
+  val parB: Par[Int] = unit(1)
 
-    "" in {
 
+  "Par 7.2" should {
+
+    "have the function map2" in {
+
+      map2(parB, parA)(_ + _)(es) shouldBe UnitFuture(3)
     }
   }
 
 
-  "Par " should {
+  "Par 7.3 - map2 with timeout" should {
 
-    "have the function map2" in {
-//      Par.map2(sum(l), sum(r))(_ + _)
+    def future[A](a:A, sleep:Long = 0) :Future[A]=  es.submit( new Callable[A] {
+      override def call(): A = {
+        Thread.sleep(sleep)
+        a
+      }})
+
+    "complete if done within timeout" in {
+
+      val par1 = timedUnit(future[Int](2, 100))
+      val par2 = timedUnit(future[Int](1, 100))
+
+      val adder = map2WithTimeout(par1, par2,  timeout = 300L, TimeUnit.MILLISECONDS)(_ + _)
+      adder(es) shouldBe UnitFuture(3)
+    }
+
+    "throw exception if not completed within timeout" in {
+
+      val par1 = timedUnit(future[Int](2,300))
+      val par2 = timedUnit(future[Int](1,400))
+
+      intercept[TimeoutException] {
+        val adder = map2WithTimeout(par1, par2, timeout = 310L, TimeUnit.MILLISECONDS)(_ + _)
+        adder(es)
+      }
+    }
+  }
+
+  "Par 7.4" should {
+
+    "asyncF" in {
+      val function = asyncF[String, Int] (string => Integer.parseInt(string))
+      val parOfStringToInt:Par[Int] = function("1")
+      val future: Future[Int] = parOfStringToInt(es)
+    }
+  }
+
+  case class Clock(start:Long) {
+    var s = start
+    def now:Long =  {
+      val r = s
+      s = s + 1
+      r
+    }
+  }
+
+  "Par 7.5" should {
+
+    "sequencce" in {
+
+      val clock: Clock = Clock(0L)
+      val ordered = sequence( List.fill(5)(unit(clock.now)) )(es)
+      val result = ordered.get(10L, TimeUnit.MILLISECONDS)
+
+      val clock2: Clock = Clock(0L)
+      result shouldBe List.fill(5)(unit(clock2.now)(es).get)
+    }
+
+    "sequence simple" in {
+
+      val clock: Clock = Clock(0L)
+      val ordered = sequence_1st_attempt( List.fill(5)(unit(clock.now)) )(es)
+      val result = ordered.get(10L, TimeUnit.MILLISECONDS)
+
+      val clock2: Clock = Clock(0L)
+      result shouldBe List.fill(5)(unit(clock2.now)(es).get)
+    }
+  }
+
+  "deadlock!! " should {
+
+    "innit" in {
+      val a = lazyUnit(42 + 1)
+      val S = Executors.newFixedThreadPool(2)
+      println(Par.equal(S)(a, fork(a)))
+
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
